cfe43817f4669554f9e0d180dd22b737cd55d54d
MD Arwaz Jamal <arwaz.jamal@codinova.com>
2021-04-22 13:56:09 +0530

    add material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,18 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { AppMainNavComponent } from './app-main-nav/app-main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './Pages/home-component/home-component.component';
 import { ProfilesComponent } from './Pages/profiles-component/profiles-component.component';
 import { RouterModule } from '@angular/router';
-import { UserProfileComponent } from './Pages/user-profile/user-profile.component';
 import { ListComponent } from './Pages/profiles-component/Observables/list/list.component';
 import { ForComponent } from './Pages/profiles-component/Observables/for/for.component';
-import {MatCardModule} from '@angular/material/card';
+import { MaterialModule } from './material/material.module';
 @NgModule({
   declarations: [
     AppComponent,
     AppMainNavComponent,
     HomeComponent,
     ProfilesComponent,
-    UserProfileComponent,
     ListComponent,
     ForComponent,
   ],
@@ -35,19 +28,13 @@ import {MatCardModule} from '@angular/material/card';
     FlexLayoutModule,
     FormsModule,
     LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatCardModule,
+    MaterialModule,
     RouterModule.forRoot([
       {path: '', component: HomeComponent},
       {path: 'profiles', component: ProfilesComponent, children: [
         {path: '', component: ListComponent},
         {path: 'from', component: ForComponent}
       ]},
-      {path: 'profile/:username', component: UserProfileComponent},
       {path: '**', component: HomeComponent}
     ])
   ],
